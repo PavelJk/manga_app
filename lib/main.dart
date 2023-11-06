8a1d47427d0277a3d523c9f9159ca6ee996b8f23
Pavel <pavel.2021@list.ru>
2023-11-06 20:08:35 +0300

    Прописан шрифт и добавлен путь к SingupPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:manga_app/pages/first_page.dart';
 import 'package:manga_app/pages/login_page.dart';
+import 'package:manga_app/pages/signup-page.dart';
 import 'package:manga_app/style/color_app.dart';
 
 void main() {
@@ -15,11 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: ColorApp.backgroundApp,
+        fontFamily: 'NotoSans'
       ),
       initialRoute: '/',
       routes: {
         '/':(context) => const FirstPage(),
         'login':(context) => const LoginPage(),
+        'signup':(context) => const SingupPage(),
       },
       debugShowCheckedModeBanner: false,
     );
