7032b7d4dc59cbf1d0fd3ccd6087f1a369e726f5
Pavel <pavel.2021@list.ru>
2023-11-08 23:37:08 +0300

    elevent переименнован и переделан на vkYandexGoogleButton

---
+++ lib/components/vkYandexGoogleButton.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:manga_app/style/color_app.dart';
+
+class VkYandexGoogleButton extends StatefulWidget {
+  final assetSvg;
+  const VkYandexGoogleButton({super.key, this.assetSvg});
+
+  @override
+  State<VkYandexGoogleButton> createState() => _VkYandexGoogleButtonState();
+}
+
+class _VkYandexGoogleButtonState extends State<VkYandexGoogleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      height: 60,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white.withOpacity(0),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(35)),
+            side: BorderSide(color: ColorApp.violet),
+          ),
+        ),
+        child: SvgPicture.asset(widget.assetSvg),
+      ),
+    );
+  }
+}
+
