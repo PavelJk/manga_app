dcc80f1b7583a83500ecdcfc4b1f9fd50b99a2e4
Pavel <pavel.2021@list.ru>
2023-11-10 00:43:32 +0300

    fixed: название класса и добавлены пробеды в return для красоты

--- lib/components/validatorEmailAndPass.dart
+++ lib/components/validatorEmailAndPass.dart
@@ -1,15 +1,15 @@
-class ValidatorChek {
+class Validator {
   String? validateEmail(String? email){
     RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
     final isEmailValid = emailRegex.hasMatch(email ?? '');
     if (!isEmailValid){
-      return 'Please enter a valid email';
+      return '      Please enter a valid email';
     }
     return null;
   }
   String? validatePassword(String? password){
-    if (password!.length < 6){
-      return 'The password must consist of 6 characters';
+    if (password!.length < 8){
+      return '      The password must consist of 8 characters';
     }
     return null;
   }
