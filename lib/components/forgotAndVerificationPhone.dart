e9080deba58af5afce88bec71d5086e7eff91186
Pavel <pavel.2021@list.ru>
2023-11-10 18:52:07 +0300

    fixed: убран виджет Align взамен добавлен rossAxisAlignment.start. Добавлено поле namePage. Проложен путь на страницу verificationCode

--- lib/components/forgotAndVerificationPhone.dart
+++ lib/components/forgotAndVerificationPhone.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:manga_app/components/validatorEmailAndPass.dart';
 import 'package:manga_app/style/color_app.dart';
 import 'package:manga_app/style/text_app.dart';
 
 class ForgotPasswordAndVerificationPhoneBody extends StatefulWidget {
-  //final namePage;
-  const ForgotPasswordAndVerificationPhoneBody({super.key, /*this.namePage*/});
+  final namePage;
+  const ForgotPasswordAndVerificationPhoneBody({super.key, this.namePage});
 
   @override
   State<ForgotPasswordAndVerificationPhoneBody> createState() => _ForgotPasswordAndVerificationPhoneBodyState();
@@ -23,23 +24,18 @@ class _ForgotPasswordAndVerificationPhoneBodyState extends State<ForgotPasswordA
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 18),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Phone verification\nEnter your mobile number',
-                        style: TextStyle(color: ColorApp.white, fontSize: 25),
-                      ),
+                    Text(
+                       '${widget.namePage}\nEnter your mobile number',
+                       style: TextStyle(color: ColorApp.white, fontSize: 25),
                     ),
                     const SizedBox(
                       height: 15,
                     ),
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'We will send you a text with a verification code',
-                        style: TextStyle(color: ColorApp.white, fontSize: 15),
-                      ),
+                    Text(
+                      'We will send you a text with a verification code',
+                      style: TextStyle(color: ColorApp.white, fontSize: 15),
                     ),
                     const SizedBox(height: 25),
                     Form(
@@ -67,7 +63,7 @@ class _ForgotPasswordAndVerificationPhoneBodyState extends State<ForgotPasswordA
                           fillColor: ColorApp.white.withOpacity(0.8),
                           hintText: '+1 000 000 0000',
                         ),
-                        validator: (numberPhone) => (numberPhone!.length < 12 || !numberPhone.contains('+')) ? '      Invalid number: + cod' : null,
+                        validator: (numberPhone) => Validator().validateNumberPhone(numberPhone),
                         keyboardType: TextInputType.phone,
                       ),
                     ),
@@ -80,7 +76,7 @@ class _ForgotPasswordAndVerificationPhoneBodyState extends State<ForgotPasswordA
                         child: ElevatedButton(
                           onPressed: () {
                             if (_formKey.currentState!.validate()) {
-                              Navigator.of(context).pushNamed('/');
+                              Navigator.of(context).pushNamed('verificationCode');
                             }
                           },
                           style: ElevatedButton.styleFrom(
