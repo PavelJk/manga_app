ef198db3cd2ad931106a2a7f385ae6bf3d0e2b94
Pavel <pavel.2021@list.ru>
2023-11-11 23:14:36 +0300

    Убраны пробелы перед предложениями

--- lib/components/validatorEmailAndPass.dart
+++ lib/components/validatorEmailAndPass.dart
@@ -3,31 +3,31 @@ class Validator {
     RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
     final isEmailValid = emailRegex.hasMatch(email ?? '');
     if (!isEmailValid){
-      return '      Please enter a valid email';
+      return '   Please enter a valid email';
     }
     return null;
   }
   String? validatePassword(String? password){
     if (password!.length < 8){
-      return '      The password must consist of 8 characters';
+      return '   The password must consist of 8 characters';
     }
     return null;
   }
   String? validateUser(String? user){
     if (user!.isEmpty) {
-      return '      Empty field';
+      return '   Empty field';
     }
     return null;
   }
   String? validateRepeatPassword(String? repeatPassword, String? firstPassword){
     if (repeatPassword != firstPassword) {
-      return '      The password doesn`t match';
+      return '   The password doesn`t match';
     }
     return null;
   }
   String? validateNumberPhone(String? numberPhone){
     if (numberPhone!.length < 12 || !numberPhone.contains('+')) {
-      return '      Invalid number: (count = 12 or >) and (+)';
+      return '   Invalid number: (count = 12 or >) and (+)';
     }
     return null;
   }
