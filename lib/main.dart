46200bf080e1eb2235956b2453d26e6ddeeb1f67
Pavel <pavel.2021@list.ru>
2024-01-10 16:19:28 +0300

    feat: initialize firebase and add the path to the home page, auth page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_app/components/errorMessange.dart';
+import 'package:manga_app/firebase_options.dart';
+import 'package:manga_app/pages/auth.dart';
 import 'package:manga_app/pages/first_page.dart';
 import 'package:manga_app/pages/forgotPassword_page.dart';
+import 'package:manga_app/pages/home_page.dart';
 import 'package:manga_app/pages/login_page.dart';
 import 'package:manga_app/pages/newPasswordPage.dart';
 import 'package:manga_app/pages/signup-page.dart';
@@ -8,7 +13,11 @@ import 'package:manga_app/pages/verification%E2%80%A8Code_page.dart';
 import 'package:manga_app/pages/verification%E2%80%A8Phone_page.dart';
 import 'package:manga_app/style/color_app.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -18,22 +27,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scaffoldMessengerKey: ErrorMessange.messangerKey,
       theme: ThemeData(
-        scaffoldBackgroundColor: ColorApp.backgroundApp,
-        fontFamily: 'NotoSans'
+          scaffoldBackgroundColor: ColorApp.backgroundApp,
+          fontFamily: 'NotoSans'
       ),
       initialRoute: '/',
       routes: {
-        '/':(context) => const FirstPage(),
-        'logIn':(context) => const LoginPage(),
-        'signUp':(context) => const SingupPage(),
-        'verificationPhone':(context) => const VerificationPhonePage(),
-        'forgotPassword':(context) => const ForgotPasswordPage(),
-        'verificationCode':(context) => const VerificationCodePage(),
-        'newPassword':(context) => const CreateNewPasswordPage(),
+        '/': (context) => const FirstPage(),
+        'auth':(context) => const Auth(),
+        'logIn': (context) => const LoginPage(),
+        'signUp': (context) => const SingUpPage(),
+        'verificationPhone': (context) => const VerificationPhonePage(),
+        'forgotPassword': (context) => const ForgotPasswordPage(),
+        'verificationCode': (context) => const VerificationCodePage(),
+        'newPassword': (context) => const CreateNewPasswordPage(),
+        'home':(context) => const HomePage(),
       },
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
